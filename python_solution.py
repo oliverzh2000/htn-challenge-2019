a05cdfab923a3351cb02344b982c60b7d6cd2c30
Oliver Zhang <oliverzh2000@gmail.com>
2019-09-14 15:46:53 -0400

    Solve challenge

--- python_solution.py
+++ python_solution.py
@@ -7,6 +7,27 @@ def refine_parameters(data: dict):
     '''
     clean_dict = {}
 
-    # Write your code here.
+    for key, value in data.items():
+        if type(value) is dict:
+            clean_dict[key] = refine_parameters(value)
+        
+        try:
+            float(value)
+            clean_dict[key] = float(value)
+        except:
+            pass
+
+        try:
+            int(value)
+            clean_dict[key] = int(value)
+        except:
+            pass
+
+        try:
+            bool(value)
+            clean_dict[key] = bool(value)
+        except:
+            pass
+
 
     return clean_dict
